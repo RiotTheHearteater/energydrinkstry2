c06d7db40a0a1034a2219145b04f352a64b90c64
Hannah <60735042+hannahcass@users.noreply.github.com>
2020-02-23 02:01:49 +0100

    code generating DrinksList.txt file

---
+++ first code.py
@@ -0,0 +1,16 @@
+DrinksList=[
+        "Monster" + " has a total of" + (" ") + str(250) + " calories",
+        "Rockstar"+ " has a total of" + (" ") + str(300) + " calories"
+        ]
+
+
+outputFile=open('DrinksList.txt','w')
+for drink in DrinksList:
+    outputFile.write (drink + "\n")
+outputFile.close()
+
+def drink(inDrinksList):
+    return drink
+
+
+print(drink("Monster"))
