c2d1e3feee5670a794b5b12b8b78e7e5e686ec62
Hannah <60735042+hannahcass@users.noreply.github.com>
2020-02-23 02:01:32 +0100

    just a play with code, does not work

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+myDrinksDictionary={
+        "Monster":["Monster", "calories"],
+        "Rockstar":["Rockstar","calories"]}
+
+#outputFile=open("myDrinksDictionary.txt")
+#outputFile.write("\n".join(myDrinksDictionary))
+#outputFile.close()
+
+#need to add this dictionary to txt outputFile
+
+#adding new drink data
+myDrinksDictionary["Name1"]=1
+
+print(myDrinksDictionary)
